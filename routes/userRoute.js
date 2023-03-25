f95614a08c3420334493350a3299af4793ee6aaa
rithchanvirakboth <rithboth0@gmail.com>
2023-03-26 03:09:17 +0700

    [backend](email-activated): apply the email activation link to backend

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const emailActivation = require('../services/emailActivation');
 const RegisterService = require('../services/register');
 
 router.post('/register', RegisterService.register);
+router.post('/acitvated', emailActivation.activateAccount);
 
 module.exports = router;
---
+++ services/emailActivation.js
@@ -0,0 +1,37 @@
+const User = require('../model/user');
+const jwt = require('jsonwebtoken');
+
+const emailActivation =  {
+  activateAccount: async (req, res) => {
+    try {
+      const { activation_token } = req.body;
+      const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET);
+      const { firstName, lastName, userName, email, password, confirmPassword } = user;
+
+      const checkEmail = await User.findOne({ email });
+      
+      if (checkEmail) {
+        return res.status(400).json({ msg: "Email already exists" });
+      }
+
+      const newUser = new User({
+        firstName,
+        lastName,
+        userName,
+        email,
+        password,
+        confirmPassword
+      });
+
+      await newUser.save();
+
+      res.json({ msg: "Account has been activated!" });
+
+    }catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  }
+}
+
+module.exports = emailActivation;
+
