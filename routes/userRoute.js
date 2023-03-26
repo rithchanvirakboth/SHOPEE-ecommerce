919214895f89cbc49ee8312684d03fcb38bdffe8
rithchanvirakboth <rithboth0@gmail.com>
2023-03-26 15:37:16 +0700

    [backend](access-token): apply getAccessTokenService for the login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 const emailActivationService = require('../services/emailActivation');
 const loginService = require('../services/login');
 const registerService = require('../services/register');
+const accessTokenService = require('../services/getAccessToken');
 
 router.post('/register', registerService.register);
 router.post('/acitvated', emailActivationService.activateAccount);
 router.post('/login', loginService.login);
+router.post('/refreshToken', accessTokenService.getAccessToken);
+
 
 
 module.exports = router;
---
+++ services/getAccessToken.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
+};
+
+const accessTokenService = {
+  getAccessToken: (req, res) => {
+    try { 
+      const refresh_token = req.cookies.refreshToken;
+      if (!refresh_token) return res.status(400).json({ message: "Please login now!" });
+
+      jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(400).json({ message: "Please login now!" });
+
+        const access_token = createAccessToken({ id: user.id });
+
+        res.json({ access_token });
+      });
+      
+    } catch (err) {
+      return res.status(500).json({ message: err.message });
+    }
+  },
+};
+module.exports = accessTokenService;
